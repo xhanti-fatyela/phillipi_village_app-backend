73b0b4264b547bfad58e2b62a96fe8adcd77eb3e
xhanti-fatyela <xmanmrcool@gmail.com>
2022-08-02 16:21:03 +0200

    added env

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,5 +1,4 @@
 
 module.exports = {
     // url: "mongodb://localhost:27017/programs_db"
-    // url: `mongodb+srv://xhanti:${process.env.PASSWORD}@cluster0.68zvwyn.mongodb.net/oppconnect?retryWrites=true&w=majority`
 }
