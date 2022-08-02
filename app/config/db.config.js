ac9e544ca188e2a86b4b02135d78439adc44cb56
xhanti-fatyela <xmanmrcool@gmail.com>
2022-08-02 15:57:15 +0200

    heroku connect

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: "mongodb://localhost:27017/programs_db"
+    // url: "mongodb://localhost:27017/programs_db"
+    url: 'mongodb+srv://xhanti:Xhanti21687@cluster0.68zvwyn.mongodb.net/oppconnect?retryWrites=true&w=majority'
 }
